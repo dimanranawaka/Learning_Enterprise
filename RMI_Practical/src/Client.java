13a914de7b6c28d1433aa360d3ed320f9a782c93
dimanranawaka <dimanranawaka@gmail.com>
2023-06-17 13:58:45 +0530

    Practical Completed

--- RMI_Practical/src/Client.java
+++ RMI_Practical/src/Client.java
@@ -1,10 +1,30 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 public class Client {
     public Client(){}
 
     public static void main(String[] args) {
-        Registry registry  = LocateRegistry.Registry
+        try {
+            Registry registry  = LocateRegistry.getRegistry(null);
+            MyFirst stub = (MyFirst) registry.lookup("myFirst");
+
+            Scanner myScan = new Scanner(System.in);
+            System.out.println("Enter x: ");
+            int x = myScan.nextInt();
+
+
+            System.out.println("Enter y: ");
+            int y = myScan.nextInt();
+
+            int answer = stub.doCal(x,y);
+            System.out.println("Answer : "+answer);
+            myScan.close();
+
+        }catch (Exception e){
+            System.err.println("Client Exception "+e.toString());
+            e.printStackTrace();
+        }
     }
 }
